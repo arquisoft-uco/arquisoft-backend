8dc34b55ec17c154d206f9680f95ada22a6a6134
DavidTamayo49 <davidtama1204@gmail.com>
2026-08-25 12:59:03 -0500

    Migration finish

--- evaluaciones/infrastructure/src/main/resources/db/migration/evaluaciones/V1.0__crear_item_cualitativo_jurado.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE item_cualitativo_jurado (
-    id UUID NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(300) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE UNIQUE INDEX uk_item_cual_jurado_nombre_ci
-    ON item_cualitativo_jurado (LOWER(nombre));
